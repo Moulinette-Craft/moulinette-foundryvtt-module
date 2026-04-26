9325c6840e51c2baf65200e2a0d3ebd486bd8749
Sven Werlen <sven.werlen@pm.me>
2026-04-26 12:37:47 -0400

    Fix Controls on V12

--- src/ts/layers/mou-layer.ts
+++ src/ts/layers/mou-layer.ts
@@ -1,17 +1,33 @@
-export default class MouLayer extends CanvasLayer {
+export default class MouLayerV12 extends ControlsLayer {
 
   override name = "MouLayer";
 
-  _draw(): this {
-    return this;
+  static override get layerOptions() {
+    return foundry.utils.mergeObject(super.layerOptions, {
+      name: "moulayer"
+    });
   }
 
-  override async draw() {
-    await super.draw();
-    return this;
+  override activate() {
+    return super.activate();
   }
 
-  override activate(): this {
-    return this;
+  _onClickLeft(event: any) {
+    
+    const t = this.worldTransform;
+    // @ts-ignore
+    const tx = (event.data.originalEvent.clientX - t.tx) / canvas.stage.scale.x;
+    // @ts-ignore
+    const ty = (event.data.originalEvent.clientY - t.ty) / canvas.stage.scale.y;
+    let coords = [tx, ty];
+    // @ts-ignore
+    coords = canvas.grid.getCenter(tx, ty);
+    console.log(coords)
+  }
+
+  _onClickRight(event: any) {
+    event;
+    // @ts-ignore
+    canvas.tiles.activate()
   }
 }
